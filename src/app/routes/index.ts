45b5f420ca3ac7b30c235f8b1f4998d9d66be352
Md. Naimur Rahman <naimurrahmanprogramming@gmail.com>
2025-07-19 23:31:58 +0600

    part 5 done

--- src/app/routes/index.ts
+++ src/app/routes/index.ts
@@ -1,8 +1,11 @@
 import { Router } from "express";
-import { UserRoutes } from "../modules/user/user.route";
 import { AuthRoutes } from "../modules/auth/auth.route";
+import { UserRoutes } from "../modules/user/user.route";
+import { DivisionRoutes } from "../modules/division/division.route";
+import { TourRoutes } from "../modules/tour/tour.route";
 
 export const router = Router();
+
 const moduleRoutes = [
     {
         path: "/user",
@@ -11,6 +14,14 @@ const moduleRoutes = [
     {
         path: "/auth",
         route: AuthRoutes,
+    },
+    {
+        path: "/division",
+        route: DivisionRoutes,
+    },
+    {
+        path: "/tour",
+        route: TourRoutes,
     },
 ];
 
